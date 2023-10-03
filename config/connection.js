340c0c329bf1a72c4f69910e733db84705aa8a14
VithusanU <130177580+VithusanU@users.noreply.github.com>
2023-10-02 23:38:12 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 const { connect, connection } = require('mongoose');
 
 //connection file
+const connectionString =
+  process.env.MONGODB_URI || '"mongodb+srv://vithusan:02716Fda48@socialapp.e8mz5ts.mongodb.net/?retryWrites=true&w=majority';
 //connect to mongoDB
-connect('');
+connect(connectionString);
 
 module.exports = connection;
